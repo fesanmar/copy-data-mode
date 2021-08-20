917326612b175beadc145f54dbb919d6ea04ac37
fesanmar <fesanmar@gmail.com>
2021-08-20 08:45:44 +0200

    Refactors and remove custom keymap

--- copy-data-mode.el
+++ copy-data-mode.el
@@ -20,27 +20,22 @@ definition. And the third one is the data. Must be a string as well"
 (defun copy-data-create-query ()
   "Creates accurate user data query string.
 Uses the `copy-data-user-snippets' variable."
-  (defun iter (snippets-list str)
-    (if snippets-list
-	(iter
-	 (cdr snippets-list)
-	 (concat str
-		 (let* ((snippet (car snippets-list))
-			(key (car snippet))
-			(description (nth 1 snippet)))
-		   (concat " ["
-			   (propertize key
-				       'face '(:foreground "red" :weight bold))
-			   "]: " description
-			   (when (cdr snippets-list) ", ")))))
-      str))
-  (iter copy-data-user-snippets "Select snippet:"))
+  (defun create-snippet-query (snippet)
+    (let ((key (car snippet))
+	  (description (nth 1 snippet)))
+      (concat " ["
+	      (propertize key
+			  'face '(:foreground "red" :weight bold))
+	      "]: " description)))
+  (concat "Select snippet:"
+	  (mapconcat 'create-snippet-query copy-data-user-snippets ", ")))
 
 (defun copy-data-query (option)
   "Push accurate data into the kill ring.
 
 Queries the user to insert the char binded to the data wanted,
-and push the accurate snippet into the kill ring.
+and push the accurate snippet into the kill ring. OPTION
+represents the snippet's key character.
 
 Snippets must be defined. For that `copy-data-user-snippets' can
 be customized.
@@ -49,19 +44,19 @@ be customized.
 		   (list
 		    (read-char (copy-data-create-query)))
 		 (error "There is no snippet yet...")))
-  (let ((option-chr (char-to-string option))
-	(found))
-    (dolist (snippet copy-data-user-snippets found)
-      (when (string-equal (car snippet) option-chr)
-	  (progn
-	    (setq found t)
-	    (kill-new (nth 2 snippet))
-	    (message "%s saved into kill ring."
-		     (nth 1 snippet)))))
-    (when (not found)
-      (message "There is no [%s] key" option-chr)
-      (sit-for 2.0)
-      (message nil))))
+  (let* ((option-chr (char-to-string option))
+	 (found-snippet (-find (lambda (snippet)
+				 (string-equal (car snippet) option-chr))
+			       copy-data-user-snippets)))
+    (if found-snippet
+	(progn
+	  (kill-new (nth 2 found-snippet))
+	  (message "%s saved into kill ring."
+		   (nth 1 found-snippet)))
+      (progn
+	(message "There is no [%s] key" option-chr)
+	(sit-for 2.0)
+	(message nil)))))
 
 ;;;###autoload
 (define-minor-mode copy-data-mode
@@ -70,9 +65,6 @@ You can store your snippets into `copy-data-user-snippets' and
 save it into your kill ring. That way, you can that snippet
 either inside or outside Emacs."
   :lighter " copy-data"
-  :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "C-c d") 'copy-data-query)
-	    map)
   :global t)
 
 (provide 'copy-data-mode)
